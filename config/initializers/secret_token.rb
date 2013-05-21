5b060d4728c64c57e65b63c2fe34c23bf5b84695
jepeloqu <jepeloqu@gmail.com>
2013-05-21 16:05:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoePeloquin::Application.config.secret_key_base = '74c58512de585640aaa24a535fb829c66d96e610d780af1f03fe6d462592b39940783a2daf4867e82b604588a0aa42336094471747bb431341cf1a73bda3670b'
